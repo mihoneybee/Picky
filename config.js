61f6a39d5181eaacd5e3a79763b869c4c7159e8f
Mel Isis <honeybee7415@gmail.com>
2026-06-16 03:53:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js - ESTE ARQUIVO NÃO VAI PARA O GITHUB
 const CONFIG = {
     SUPABASE_URL: 'https://omdsuzrlrcnkjohqudzw.supabase.co',
     SUPABASE_KEY: 'sb_publishable_e6mW7MwJ_qrzyfK6UyUyug_6-DVG3Gx',
