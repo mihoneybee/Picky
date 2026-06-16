b0f54be6f534b98482b25dad6ba24b646dced60c
Mel Isis <honeybee7415@gmail.com>
2026-06-16 03:19:33 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js - ESTE ARQUIVO NÃO VAI PARA O GITHUB
+const CONFIG = {
+    SUPABASE_URL: 'https://omdsuzrlrcnkjohqudzw.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_e6mW7MwJ_qrzyfK6UyUyug_6-DVG3Gx',
+    GEMINI_KEY: 'AQ.Ab8RN6L7FRu-FPjdVCGz2Ng50v4BXhzaHaE63siejlQ2SLoPBQ'
+};
