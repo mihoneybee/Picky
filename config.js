cb24a9a2f19314c051169afc5e6ebb984544b702
Mel Isis <honeybee7415@gmail.com>
2026-06-17 23:35:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     SUPABASE_URL: 'https://omdsuzrlrcnkjohqudzw.supabase.co',
     SUPABASE_KEY: 'sb_publishable_e6mW7MwJ_qrzyfK6UyUyug_6-DVG3Gx',
-    GEMINI_KEY: 'AQ.Ab8RN6L7FRu-FPjdVCGz2Ng50v4BXhzaHaE63siejlQ2SLoPBQ'
+    GEMINI_KEY: 'AQ.Ab8RN6IyoNHpOpvX-QgX1ofNMsqxFudSvNJyg-lIEz-GosuPnQ'
 };
